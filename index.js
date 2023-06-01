a3bbbef76abd1b99b3d525a91870eaf28358d1fc
chiragautomotive <95574219+chiragautomotive@users.noreply.github.com>
2023-06-01 20:07:08 +0530

    Add files via upload
    initial

---
+++ index.js
@@ -0,0 +1,7 @@
+function getPhoneNumber(){
+            // Selecting the input element and get its value 
+            var inputVal = 91+document.getElementById("phoneNumber").value;
+        var walink ="https://wa.me/"+inputVal
+ 
+  window.open("https://wa.me/"+inputVal+"?text=*Dear%20Patron,*%0A%0AGreetings%20from%20*Chirag%20Green%20Mobility!!*%0A%0AThank%20you%20for%20your%20interest%20in%20*Tork%20Motors%20Kratos-R%20(India%E2%80%99s%20First%20Electric%20Motorcycle).*%0A%0ATo%20experience%20the%20thrill%20of%20the%20Kratos-R,%20please%20contact%20us%20and%20our%20representative%20will%20assist%20you%20in%20scheduling%20a%20test%20ride%20appointment.%0A%0AFeel%20free%20to%20contact%20us%20for%20any%20further%20queries.%0A*9173100453*%0A%0AThanks%20again!")
+}
